512b49e41858039affea4d72fb2adbdbfe1ae9ad
Oleksii Ostapov <ostapov@gmail.com>
2021-07-20 17:50:01 +0300

    initial commit

---
+++ src/cmslink.js
@@ -0,0 +1,38 @@
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview'
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin'
+import LinkUI from '@ckeditor/ckeditor5-link/src/linkui'
+import searchIcon from 'ckeditor5-cmslink/theme/icons/search.svg'
+
+class Cmslink extends Plugin {
+	init() {
+		const editor = this.editor
+		const linkUI = editor.plugins.get(LinkUI)
+
+		this.linkFormView = linkUI.formView
+		this.button = this._createButton()
+
+		this.linkFormView.once('render', () => {
+			this.button.render()
+			this.linkFormView.registerChild(this.button)
+			this.linkFormView.element.insertBefore(this.button.element, this.linkFormView.saveButtonView.element)
+		})
+	}
+
+	_createButton() {
+		const button = new ButtonView( this.locale )
+
+		button.set({
+      icon: searchIcon,
+			label: 'Internal link',
+			withText: true,
+			tooltip: true
+		})
+
+		button.on('execute', () => {
+      alert('execute')
+			this.linkFormView.urlInputView.value = 'http://some.internal.link'
+		})
+
+		return button
+	}
+}
